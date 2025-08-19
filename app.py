15d564f3e7e612a46377ffc07d69904382e72eff
Frances Belleza <francesanne.belleza@sjsu.edu>
2025-08-19 12:36:43 -0700

    added a scatterplotter for total runs

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from tools.missteps import missteps_scatter_tool
+from tools.steps import steps_scatter_tool
 
 def main():
     st.title("Lab Tools")
@@ -12,12 +13,15 @@ def main():
     if st.session_state.tool is None:
         if st.button("missteps scatterplots"):
             st.session_state.tool = "missteps"
+        if st.button("steps scatterplots"):
+            st.session_state.tool = "steps"
         ####add for future button
 
     # 3) dispatch
     if st.session_state.tool == "missteps":
         missteps_scatter_tool()
+    elif st.session_state.tool == "steps":
+        steps_scatter_tool()
 
 if __name__ == "__main__":
     main()
-
